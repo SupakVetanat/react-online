175e2a8aff15dc9ced78c0795f5b7fb9b33e175f
SupakVetanat <ve.supak_st@tni.ac.th>
2022-01-29 10:26:35 +0700

    Ch7#1

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ import CreatePage from "./pages/category/CreatePage";
 import EditPage from "./pages/category/EditPage";
 import UploadPage from "./pages/UploadPage";
 import { ToastProvider} from 'react-toast-notifications';
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 
 function App() {
@@ -33,6 +35,8 @@ function App() {
             <Route path='/contact'><ContactPage/></Route>
             <Route path='/hospital'><HospitalPage/></Route>
             <Route path='/upload'><UploadPage></UploadPage></Route>
+            <Route path='/register'><RegisterPage/></Route>
+            <Route path='/login'><LoginPage/></Route>
             {/*<Route path='/category'><indexPage/></Route>*/}
             <Route path='/category' render={ ({match : {url}}) => (
               <>
